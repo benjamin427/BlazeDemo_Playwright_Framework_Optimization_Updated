ee7adac4ea6d9a8a16c49d6697a4cf38b3cf5a84
benjamin427 <shadowboxer79@hotmail.com>
2026-08-12 15:54:09 -0400

    Made modifications for a working module

--- tests/production/smoke/register.spec.js
+++ tests/production/smoke/register.spec.js
@@ -1,4 +1,4 @@
-import {blazedemo_register} from '/Users/Shado/Documents/BlazeDemo_Playwright_Updated/environments/production/smoke/pageObjects/register/register'
+import {blazedemo_register} from '../../../environments/production/smoke/pageObjects/register/register'
 import {test, expect} from '@playwright/test'
 import logger from '../../../utils/logger'
 
