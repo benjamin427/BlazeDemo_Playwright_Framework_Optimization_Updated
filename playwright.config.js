5fa7e7355877dfab6f815c3ec8524a4876815799
benjamin427 <shadowboxer79@hotmail.com>
2026-08-13 11:06:46 -0400

    Adjusted global timeout for increasing time for test execution

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import path from 'path'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalTimeout: 600000,
+  globalTimeout: 75000,
   timeout: 90000,
   expect: {
     timeout: 15000
