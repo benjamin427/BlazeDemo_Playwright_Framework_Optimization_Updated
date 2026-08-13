69b3a081c7841733d32394670bbed8d63ded6c18
benjamin427 <shadowboxer79@hotmail.com>
2026-08-13 11:13:21 -0400

    Adjusted global timeout to increase time for finding elements

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import path from 'path'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalTimeout: 75000,
+  globalTimeout: 77000,
   timeout: 90000,
   expect: {
     timeout: 15000
