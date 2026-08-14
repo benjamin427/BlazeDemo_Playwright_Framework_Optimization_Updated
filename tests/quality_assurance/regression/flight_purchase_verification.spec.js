dff8bcf69bb6251ffe7cdbb30591234569419324
benjamin427 <shadowboxer79@hotmail.com>
2026-08-13 21:05:06 -0400

    Made modifications for this file

--- tests/quality_assurance/regression/flight_purchase_verification.spec.js
+++ tests/quality_assurance/regression/flight_purchase_verification.spec.js
@@ -11,17 +11,17 @@ test.beforeEach(async({page}) => {
 })
 
 test("Select city for departure and for destination to get flight information", async({page}) => {
-    const departure = new blazedemo_menuSelectDeparture(page)
-    const destination = new blazedemo_menuSelectDestination(page)
+    const departure_1 = new blazedemo_menuSelectDeparture(page)
+    const destination_1 = new blazedemo_menuSelectDestination(page)
     const reserve_flights = new blazedemo_reserve_flights(page)
     const referenceReserveURL = process.env.BLAZEDEMO_RESERVE_VIRGIN_AMERICA_URL_ENDPOINT
     const referenceURL = process.env.BLAZEDEMO_PURCHASE_VIRGIN_AMERICA_URL_ENDPOINT
     // Select departure city from drop down menu
-    await departure.menuSelectDeparturePhiladelphia()
+    await departure_1.menuSelectDeparturePhiladelphia()
     // Select destinatuon city from drop down menu
-    await destination.menuSelectDestinationNewYork()
+    await destination_1.menuSelectDestinationNewYork()
     // Click button to find flights
-    await destination.clickFindFlights()
+    await destination_1.clickFindFlights()
     // Verify the url endpoint after clicking the button to navigate to the next page
     await expect(page).toHaveURL(referenceReserveURL)
     // Select airline service to purchase flight ticket
@@ -29,27 +29,27 @@ test("Select city for departure and for destination to get flight information", 
 })
 
 test("Select city for departure and destination to make a purchase for United Airlines", async({page}) => {
-    const departure = new blazedemo_menuSelectDeparture(page)
-    const destination = new blazedemo_menuSelectDestination(page)
+    const departure_2 = new blazedemo_menuSelectDeparture(page)
+    const destination_2 = new blazedemo_menuSelectDestination(page)
     const reserve_flights = new blazedemo_reserve_flights(page)
     const referenceReserveURL = process.env.BLAZEDEMO_RESERVE_UNITED_AIRLINES_URL_ENDPOINT
     const referenceURL = process.env.BLAZEDEMO_PURCHASE_UNITED_AIRLINES_URL_ENDPOINT
-    await departure.menuSelectDepartureSanDiego()
-    await destination.menuSelectDestinationCairo()
-    await destination.clickFindFlights()
+    await departure_2.menuSelectDepartureSanDiego()
+    await destination_2.menuSelectDestinationCairo()
+    await destination_2.clickFindFlights()
     await expect(page).toHaveURL(referenceReserveURL)
     await reserve_flights.selectAerLingus_Flight_9696()
     await expect(page).toHaveURL(referenceURL)
 })
 
 test.afterEach(async({page}, testInfo) => {
-    const destination = new blazedemo_menuSelectDestination(page)
+    const destination_3 = new blazedemo_menuSelectDestination(page)
     // Verify the status of test results
     if (testInfo.status === 'failed') {
         logger.error(`Test failed at step: "${testInfo.title}"`)
     } else {
         logger.info(`Test complete with status: ${testInfo.status.toUpperCase()} at step: ${testInfo.title}`)
     }
-    await destination.closeBrowser()
+    await destination_3.closeBrowser()
 })
 
