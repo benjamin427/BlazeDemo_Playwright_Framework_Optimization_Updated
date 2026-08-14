877ee32df0073bfb6931c230689f8f96c28b403a
benjamin427 <shadowboxer79@hotmail.com>
2026-08-14 10:09:55 -0400

    Changed the timeout setting for complete test execution

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import path from 'path'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalTimeout: 490000,
+  globalTimeout: 550000,
   timeout: 120000,
   expect: {
     timeout: 15000
