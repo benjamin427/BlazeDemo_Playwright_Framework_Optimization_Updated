85fe477fe3ecca606979589d22329c2914df5eb7
benjamin427 <shadowboxer79@hotmail.com>
2026-08-20 12:07:42 -0400

    Adjusted timeout settings

--- playwright.config.js
+++ playwright.config.js
@@ -15,7 +15,7 @@ import path from 'path'
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  globalTimeout: 60000,
+  globalTimeout: 70000,
   timeout: 25000,
   expect: {
     timeout: 35000
