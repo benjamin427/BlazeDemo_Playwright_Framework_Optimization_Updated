d3e74ff4bf7a314026b7a908eb9b62f568b80827
benjamin427 <shadowboxer79@hotmail.com>
2026-08-24 13:08:20 -0400

    Made slight modifications on some lines of code

--- tests/quality_assurance/mock_api_tests/api_test.spec.js
+++ tests/quality_assurance/mock_api_tests/api_test.spec.js
@@ -1,7 +1,6 @@
 import {test, expect} from '@playwright/test'
 import logger from '../../../utils/logger'
 
-
 test.beforeEach(() => {
     logger.info('tests/quality_assurance/mock_api_tests/api_test.spec.js: Start test execution initialization...')
 })
